fdbd58b48678e4167ace606ffa27012e181732a7
frjaraur <frjaraur@gmail.com>
2018-07-28 10:57:24 +0200

    First Commit, added clone.sh script

---
+++ Readme.md
@@ -0,0 +1,8 @@
+## VMWare ESXi Tools
+
+### Clone
+
+~~~
+./clone.sh source_vm cloned_vm
+~~~
+This will create a new directory with the cloned contents (disk [just one disk at this time] and config file) and will register the newly created vm on ESXi.
---
+++ clone.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+src=$1
+shift
+dest=$1
+shift
+options="$@"
+
+datastore=/vmfs/volumes/datastore1
+
+ls -d ${datastore}/${src} > /dev/null 2>&1 \
+|| ( echo "VM ${src} does not exist... exiting" \
+&& exit 2 )
+
+ls -d ${datastore}/${dest} > /dev/null 2>&1 \
+&& echo "VM ${dest} exists, must be removed first... exiting" \
+&& exit 2
+
+mkdir -p ${datastore}/${dest}
+
+vmkfstools -i ${datastore}/${src}/${src}.vmdk \
+${datastore}/${dest}/${dest}.vmdk -d thin
+
+grep -v uuid ${datastore}/${src}/${src}.vmx | sed -e "s/${src}/${dest}/g" > \
+${datastore}/${dest}/${dest}.vmx 
+
+vim-cmd solo/registervm ${datastore}/${dest}/${dest}.vmx
+
