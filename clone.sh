8d4dc7c6f31be0d1576257b7300dab3bbdd2187e
Javier Ramirez <frjaraur@gmail.com>
2018-08-27 17:00:41 +0200

    Fix empty source or destination

--- clone.sh
+++ clone.sh
@@ -5,23 +5,30 @@ dest=$1
 shift
 options="$@"
 
+[ ! -n "${src}" ] && echo "Invalid Source Name" && exit 1
+[ ! -n "${dest}" ] && echo "Invalid Dest Name" && exit 1
+
 datastore=/vmfs/volumes/datastore1
 
-ls -d ${datastore}/${src} > /dev/null 2>&1 \
-|| echo "VM ${src} does not exist... exiting" \
-&& exit 2 
+ls -d ${datastore}/${src} > /dev/null 2>&1 
+if [ $? -ne 0 ] 
+then
+	echo "VM ${src} does not exist... exiting" 
+	exit 2 
+fi
 
-ls -d ${datastore}/${dest} > /dev/null 2>&1 \
-&& echo "VM ${dest} exists, must be removed first... exiting" \
-&& exit 2
+ls -d ${datastore}/${dest} > /dev/null 2>&1 
+if [ $? -eq 0 ]
+then
+	echo "VM ${dest} exists, must be removed first... exiting" 
+	exit 2
+fi
 
 mkdir -p ${datastore}/${dest}
 
 vmkfstools -i ${datastore}/${src}/${src}.vmdk \
 ${datastore}/${dest}/${dest}.vmdk -d thin
 
-grep -v uuid ${datastore}/${src}/${src}.vmx | sed -e "s/${src}/${dest}/g" > \
-${datastore}/${dest}/${dest}.vmx 
+grep -v "uuid" ${datastore}/${src}/${src}.vmx | sed -e "s/${src}/${dest}/g" > ${datastore}/${dest}/${dest}.vmx 
 
-vim-cmd solo/registervm ${datastore}/${dest}/${dest}.vmx
-
+vim-cmd solo/registervm ${datastore}/${dest}/${dest}.vmx >/dev/null
